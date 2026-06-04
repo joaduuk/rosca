97e62ccdebab6b552ffefa0906e9369863f06b86
joaduuk <joaduuk@yahoo.co.uk>
2026-06-04 19:49:17 +0100

    final

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,11 +21,14 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
+        "https://roscaapp.com",
+        "https://www.roscaapp.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(auth.router)
 app.include_router(users.router)
