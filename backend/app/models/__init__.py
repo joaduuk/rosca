f359c26b5b2c9920d85806a8a87f12210e6e10d1
root <root@srv1612220.hstgr.cloud>
2026-07-06 22:50:37 +0000

    Add blog_posts table + register BlogPost in models __init__

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,7 +3,6 @@ from .group import Group
 from .membership import Membership
 from .contribution import Contribution
 from .payout import PayoutSchedule
-
 from app.models.notification import Notification
-
-__all__ = ["User", "Group", "Membership", "Contribution", "PayoutSchedule", "Notification"]
+from .blog_post import BlogPost
+__all__ = ["User", "Group", "Membership", "Contribution", "PayoutSchedule", "Notification", "BlogPost"]
